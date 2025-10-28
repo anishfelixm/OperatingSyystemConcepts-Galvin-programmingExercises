44799180b15555fc869c77ece59a94de00ab94e5
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-28 15:24:52 +0530

    feat : Completed programming project partially

--- Chapter-2_Operating-system-structures/ProgrammingProjects/simple.c
+++ Chapter-2_Operating-system-structures/ProgrammingProjects/simple.c
@@ -17,11 +17,16 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/gcd.h> /* for gcd() */
+#include <asm/param.h> /* for HZ variable which holds the Timer interrupt duration value */
+#include <linux/jiffies.h> /* for jiffies variable which maintains the total number of timer interrupts that happened after system was booted */
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
-       printk(KERN_INFO "Loading Custom Module\n");
+       printk(KERN_INFO"Loading Custom Module\n");
+       printk(KERN_INFO"The Golden ratio: %lu\n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO"The value of HZ: %lu, The value of jiffies: %lu\n", HZ, jiffies);
 
        return 0;
 }
@@ -29,6 +34,9 @@ static int simple_init(void)
 /* This function is called when the module is removed. */
 static void simple_exit(void) {
 	printk(KERN_INFO "Removing Custom Module\n");
+       unsigned long a = 3300, b = 24, c = gcd(a, b);
+       printk(KERN_INFO"The gcd of %lu and %lu: %lu\n", a, b, c);
+       printk(KERN_INFO"The value of jiffies: %lu\n", jiffies);
 }
 
 /* Macros for registering module entry and exit points. */
