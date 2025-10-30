fdd95813ea8fce9abac4fc8d25594b81d8004caa
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-30 11:20:04 +0530

    feat : Completed Q1 of programming projects of chapter2

---
+++ Chapter-2_Operating-system-structures/ProgrammingProjects/Q1.c
@@ -0,0 +1,50 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/jiffies.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "jiffies"
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+
+static struct proc_ops proc_ops = {
+        .proc_read = proc_read,
+};
+
+static int proc_init(void) {
+    proc_create(PROC_NAME, 0, NULL, &proc_ops);
+    printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+    return 0;
+}
+
+static void proc_exit(void) {
+    remove_proc_entry(PROC_NAME, NULL);
+    printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
+    int rv = 0;
+    char buffer[BUFFER_SIZE];
+    static int completed = 0;
+
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+
+    completed = 1;
+    rv = sprintf(buffer, "jiffies: %lu\n", jiffies);
+    
+    copy_to_user(usr_buf, buffer, rv);
+    return rv;
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Display jiffies in /proc/jiffies");
+MODULE_AUTHOR("SGG");
